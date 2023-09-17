e5e5e2d4c5695d4c40eb075c57cd6132a8ae64bc
guilherme <gui-sperandio@hotmail.com>
2023-09-16 23:58:23 -0300

    feat: fazendo o backend da aplicação do nlw ai

---
+++ prisma/migrations/20230917011523_rename_video_field/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `creatdAt` on the `Video` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Video" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "transcription" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_Video" ("id", "name", "path", "transcription") SELECT "id", "name", "path", "transcription" FROM "Video";
+DROP TABLE "Video";
+ALTER TABLE "new_Video" RENAME TO "Video";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
